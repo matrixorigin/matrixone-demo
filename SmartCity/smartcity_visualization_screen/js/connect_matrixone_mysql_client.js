7d7fbf75f51c22f3934a629a8771e61c47bf61c6
NingXinyu <nxyqdl@163.com>
2022-09-27 15:54:56 +0800

    by JayceTalis

--- SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_client.js
+++ SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_client.js
@@ -4,10 +4,15 @@
 
 window.falldown_data;
 
+//mysql_read_falldown：摔倒数据读取函数，读取falldown表中最近一条数据，如果该数据与当前时间间隔小于2000秒则显示告警，否则显示正常。
+
 function mysql_read_falldown() {   
     b=$.ajax({
     type:'get', //请求方式
-    url:'http://192.168.194.94:3000/falldown',  //请求地址,需要根据自己服务器ip地址来替换
+    //MatrixONE MySQL地址
+    //url:'http://192.168.194.94:3000/falldown',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    url:'http://192.168.194.94:3001/falldown',
     success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
         console.log(result)
         window.falldown_data = result;
@@ -26,7 +31,15 @@ function mysql_read_falldown() {
             fall_warning.onclick = function () {
                 //alert("警告");  //替换文本
                 var img = document.getElementById("outimg");
-                img.src=result[0].raw;
+                //img.src=result[0].raw; //base64
+                //二进制
+                // const objectURL = URL.createObjectURL(result[0].raw);
+                // img.src=objectURL;
+                const binaryData = [];
+                binaryData.push(result[0].raw);
+                url = window.URL.createObjectURL(new Blob(binaryData,{type:'application/pdf;chartset=UTF-8'}));
+                img.src=url;
+
             }
         }else{
             fall_warning.style="color:rgb(88, 253, 94)";
