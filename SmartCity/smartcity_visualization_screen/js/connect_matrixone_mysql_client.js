480f4713ae7c49a24b87a29ce85da6b0ed117da9
NingXinyu <nxyqdl@163.com>
2022-09-30 00:18:16 +0800

    by JayceTalis

--- SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_client.js
+++ SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_client.js
@@ -10,12 +10,12 @@ function mysql_read_falldown() {
     b=$.ajax({
     type:'get', //请求方式
     //MatrixONE MySQL地址
-    //url:'http://192.168.194.94:3000/falldown',  //请求地址,需要根据自己服务器ip地址来替换
+    url:'http://10.112.89.158:3000/falldown',  //请求地址,需要根据自己服务器ip地址来替换
     //MySQL地址
-    url:'http://192.168.194.94:3001/falldown',
+    //url:'http://192.168.194.94:3001/falldown',
     success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
-        console.log(result)
-        window.falldown_data = result;
+        console.log('falldown:',result)
+        //window.falldown_data = result;
         var myDate = new Date();
         // myDate.getTime();
         // console.log(myDate.getTime());
@@ -35,10 +35,17 @@ function mysql_read_falldown() {
                 //二进制
                 // const objectURL = URL.createObjectURL(result[0].raw);
                 // img.src=objectURL;
-                const binaryData = [];
-                binaryData.push(result[0].raw);
-                url = window.URL.createObjectURL(new Blob(binaryData,{type:'application/pdf;chartset=UTF-8'}));
-                img.src=url;
+
+                // const binaryData = [];
+                // binaryData.push(result[0].raw);
+                // url = window.URL.createObjectURL(new Blob(binaryData,{type:'application/pdf;chartset=UTF-8'}));
+                // img.src=url;
+
+                //const buffer = new Buffer(result[0].raw, 'binary');
+                //img.src = 'data: image/'+ getImageType(fileName) +';base64,' + buffer.toString('base64');
+                console.log(result[0].raw)
+                const base64 = blobToDataURI(result[0].raw);
+                img.src = 'data: image/jpg;base64,' + String(base64);
 
             }
         }else{
@@ -52,4 +59,215 @@ function mysql_read_falldown() {
           });
 setTimeout("mysql_read_falldown()", 10000);
 }
+//二进制转base64
+// function blobToDataURI(blob, callback) {
+//     var reader = new FileReader();
+//     reader.readAsDataURL(blob);
+//     reader.onload = function (e) {
+//         callback(e.target.result);
+//     }
+//  }
+
+//mysql_read_face：人脸数据读取函数
+function mysql_read_face() {   
+    b=$.ajax({
+    type:'get', //请求方式
+    //MatrixONE MySQL地址
+    url:'http://10.112.89.158:3000/face',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    //url:'http://192.168.194.94:3001/falldown',
+    success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
+        console.log('face:',result)
+        window.falldown_data = result;
+        var myDate = new Date();
+        //console.log(myDate.getTime()/1000-result[0].time);
+        fall_warning = document.getElementById("falldown");
+        if(myDate.getTime()/1000-result[0].time<2000){
+            fall_warning.style="color:rgb(253, 88, 88)";
+            fall_warning.innerText="告警";
+            //console.log(result[0].raw)
+            //点击触发事件
+            fall_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("outimg");
+                img.src=result[0].raw; //base64
+            }
+        }else{
+            fall_warning.style="color:rgb(88, 253, 94)";
+            fall_warning.innerText="正常";
+        }
+            },
+    error:function (xhr){  //请求失败后调用
+    console.log(xhr);
+            }
+          });
+setTimeout("mysql_read_face()", 10000);
+}
+
+//mysql_read_well：井盖数据读取函数
+function mysql_read_well() {   
+    b=$.ajax({
+    type:'get', //请求方式
+    //MatrixONE MySQL地址
+    url:'http://10.112.89.158:3000/well',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    //url:'http://192.168.194.94:3001/falldown',
+    success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
+        console.log('well:',result)
+        //window.falldown_data = result;
+        var myDate = new Date();
+        //console.log(myDate.getTime()/1000-result[0].time);
+        well_warning = document.getElementById("well");
+        if(myDate.getTime()/1000-result[0].time>000){
+            well_warning.style="color:rgb(253, 88, 88)";
+            well_warning.innerText="告警";
+            //console.log(result[0].raw)
+            //点击触发事件
+            well_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("center_outimg");
+                img.src=result[0].raw; //base64
+            }
+        }else{
+            well_warning.style="color:rgb(88, 253, 94)";
+            wel_warning.innerText="正常";
+        }
+            },
+    error:function (xhr){  //请求失败后调用
+    console.log(xhr);
+            }
+          });
+setTimeout("mysql_read_well()", 10000);
+}
+
+//mysql_read_multiobject：多目标数据读取函数
+function mysql_read_multiobject() {   
+    b=$.ajax({
+    type:'get', //请求方式
+    //MatrixONE MySQL地址
+    url:'http://10.112.89.158:3000/multiobject',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    //url:'http://192.168.194.94:3001/falldown',
+    success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
+        console.log('multiobject:',result)
+        window.falldown_data = result;
+        var myDate = new Date();
+        //console.log(myDate.getTime()/1000-result[0].time);
+        hat_warning = document.getElementById("hat");
+        if(myDate.getTime()/1000-result[0].time>000){
+           
+            //点击触发事件
+            hat_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("center_outimg");
+                img.src=result[0].raw; //base64
+            }
+        }else{
+            
+        }
+            },
+    error:function (xhr){  //请求失败后调用
+    console.log(xhr);
+            }
+          });
+setTimeout("mysql_read_multiobject()", 10000);
+}
+
+//mysql_read_smokefire：烟火数据读取函数
+function mysql_read_smokefire() {   
+    b=$.ajax({
+    type:'get', //请求方式
+    //MatrixONE MySQL地址
+    url:'http://10.112.89.158:3000/smokefire',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    //url:'http://192.168.194.94:3001/falldown',
+    success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
+        console.log('smokefire:',result)
+        //console.log('smokefire:',result.environment)
+        var env = String(result.environment).split(',');
+        //console.log('smokefire:',env);
+        console.log('fire:',env[0].slice(10,13));
+        console.log('smoke:',env[1].slice(11,14));
+        //window.falldown_data = result;
+        var myDate = new Date();
+        //console.log(myDate.getTime()/1000-result[0].time);
+        fire_warning = document.getElementById("fire");
+        smoke_warning = document.getElementById("smoke");
+        if(env[1].slice(11,14)>0.5){
+            smoke_warning.style="color:rgb(253, 88, 88)";
+            smoke_warning.innerText="告警";
+            //console.log(result[0].raw)
+            //点击触发事件
+            smoke_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("center_outimg");
+                img.src=result.raw; //base64
+            }
+        }else{
+            smoke_warning.style="color:rgb(88, 253, 94)";
+            smoke_warning.innerText="正常";
+        }
+        if(env[0].slice(10,13)>0.5){
+            fire_warning.style="color:rgb(253, 88, 88)";
+            fire_warning.innerText="告警";
+            //console.log(result[0].raw)
+            //点击触发事件
+            fire_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("center_outimg");
+                img.src=result.raw; //base64
+            }
+        }else{
+            fire_warning.style="color:rgb(88, 253, 94)";
+            fire_warning.innerText="正常";
+        }
+        
+            },
+    error:function (xhr){  //请求失败后调用
+    console.log(xhr);
+            }
+          });
+setTimeout("mysql_read_smokefire()", 10000);
+}
+
+//mysql_read_vehicle：车辆数据读取函数
+function mysql_read_vehicle() {   
+    b=$.ajax({
+    type:'get', //请求方式
+    //MatrixONE MySQL地址
+    url:'http://10.112.89.158:3000/vehicle',  //请求地址,需要根据自己服务器ip地址来替换
+    //MySQL地址
+    //url:'http://192.168.194.94:3001/falldown',
+    success:function(result) {  //请求成功以后函数被调用  response为服务器返回数据 该方法内部会将json字符串转为json对象
+        console.log('vehicle:',result)
+        window.falldown_data = result;
+        var myDate = new Date();
+        //console.log(myDate.getTime()/1000-result[0].time);
+        fall_warning = document.getElementById("falldown");
+        if(myDate.getTime()/1000-result[0].time>000){
+            fall_warning.style="color:rgb(253, 88, 88)";
+            fall_warning.innerText="告警";
+            //console.log(result[0].raw)
+            //点击触发事件
+            fall_warning.onclick = function () {
+                //alert("警告");  //替换文本
+                var img = document.getElementById("outimg");
+                img.src=result[0].raw; //base64
+            }
+        }else{
+            fall_warning.style="color:rgb(88, 253, 94)";
+            fall_warning.innerText="正常";
+        }
+            },
+    error:function (xhr){  //请求失败后调用
+    console.log(xhr);
+            }
+          });
+setTimeout("mysql_read_vehicle()", 10000);
+}
 mysql_read_falldown();
+mysql_read_face();
+mysql_read_well();
+mysql_read_multiobject();
+mysql_read_smokefire();
+mysql_read_vehicle();
--- SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_server.js
+++ SmartCity/smartcity_visualization_screen/js/connect_matrixone_mysql_server.js
@@ -4,10 +4,6 @@
 
 //2.在你所选文件夹终端中执行 npm install mysql安装MySQL模块
 
-// 下载express 执行 npm install express
-
-// 下载MySQL 执行 npm install mysql
-
 var express = require('express');
 var ap_p = express();
 ap_p.all('*', function(req, res, next) {             //设置跨域访问
@@ -18,6 +14,7 @@ ap_p.all('*', function(req, res, next) {             //设置跨域访问
      res.header("Content-Type", "application/json;charset=utf-8");
      next();
   });
+// 摔倒falldown记录查询
 ap_p.get('/falldown',function(req,res){           //配置接口api
     res.status(200);
     var mysql  = require('mysql');  
@@ -42,7 +39,133 @@ ap_p.get('/falldown',function(req,res){           //配置接口api
     });
     //connection.end();    
 });
+// 人脸face记录查询
+ap_p.get('/face',function(req,res){           //配置接口api
+    res.status(200);
+    var mysql  = require('mysql');  
+    console.log('mysql_face')
+    var connection = mysql.createConnection({     
+      host     : 'localhost',       
+      user     : 'dump',              
+      password : '111',       
+      port: '6001',                   
+      database: 'park' 
+    }); 
+     connection.connect();
+    var  sql = 'SELECT * FROM face ORDER BY `time` DESC';
+    connection.query(sql,function (err, result) {
+            if(err){
+              console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+           // re1=result[result.length-1];
+           re1=result
+           res.json(re1);
+    });
+    //connection.end();    
+});
+// 井盖manhole记录查询
+ap_p.get('/well',function(req,res){           //配置接口api
+    res.status(200);
+    var mysql  = require('mysql');  
+    console.log('mysql_well')
+    var connection = mysql.createConnection({     
+      host     : 'localhost',       
+      user     : 'dump',              
+      password : '111',       
+      port: '6001',                   
+      database: 'park' 
+    }); 
+     connection.connect();
+    var  sql = 'SELECT * FROM well ORDER BY `time` DESC';
+    connection.query(sql,function (err, result) {
+            if(err){
+              console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+           // re1=result[result.length-1];
+           re1=result
+           res.json(re1);
+    });
+    //connection.end();    
+});
+// 多目标multiobject记录查询
+ap_p.get('/multiobject',function(req,res){           //配置接口api
+    res.status(200);
+    var mysql  = require('mysql');  
+    console.log('mysql_multiobject')
+    var connection = mysql.createConnection({     
+      host     : 'localhost',       
+      user     : 'dump',              
+      password : '111',       
+      port: '6001',                   
+      database: 'park' 
+    }); 
+     connection.connect();
+    var  sql = 'SELECT * FROM multiobject ORDER BY `time` DESC';
+    connection.query(sql,function (err, result) {
+            if(err){
+              console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+           // re1=result[result.length-1];
+           re1=result
+           res.json(re1);
+    });
+    //connection.end();    
+});
+// 烟火smokefire记录查询
+ap_p.get('/smokefire',function(req,res){           //配置接口api
+    res.status(200);
+    var mysql  = require('mysql');  
+    console.log('mysql_smokefire')
+    var connection = mysql.createConnection({     
+      host     : 'localhost',       
+      user     : 'dump',              
+      password : '111',       
+      port: '6001',                   
+      database: 'park' 
+    }); 
+     connection.connect();
+    var  sql = 'SELECT * FROM smokefire ORDER BY `time` DESC';
+    connection.query(sql,function (err, result) {
+            if(err){
+              console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+           re1=result[result.length-1];
+           //re1=result
+           res.json(re1);
+    });
+    //connection.end();    
+});
+// 车辆vehicle记录查询
+ap_p.get('/vehicle',function(req,res){           //配置接口api
+    res.status(200);
+    var mysql  = require('mysql');  
+    console.log('mysql_vehicle')
+    var connection = mysql.createConnection({     
+      host     : 'localhost',       
+      user     : 'dump',              
+      password : '111',       
+      port: '6001',                   
+      database: 'park' 
+    }); 
+     connection.connect();
+    var  sql = 'SELECT * FROM vehicle ORDER BY `time` DESC';
+    connection.query(sql,function (err, result) {
+            if(err){
+              console.log('[SELECT ERROR] - ',err.message);
+              return;
+            }
+           // re1=result[result.length-1];
+           re1=result
+           res.json(re1);
+    });
+    //connection.end();    
+});
 var server = ap_p.listen(3000,function(){
     var host = server.address().address;
     var port = server.address().port;
 });
+
